52f565f0be77f70dd0db15e11334e2bbc0327d17
amir <amirtaki623@gmail.com>
2025-05-12 19:47:03 +0330

    add js

--- src/javascript.js
+++ src/javascript.js
@@ -1,69 +1,40 @@
-
-
-document.querySelector('.container_i').addEventListener('click', (e) =>{
-
-    if (e.target.nodeName != 'I') return ;
-
-    let data ;
-
-    switch (e.target.getAttributeNode('item-id').value){
-
-        case 'home':
-        
-            data = {title : "HOME" , color : 'red' , itemId : "home"}
-            update (data)
-            history.pushState(data, "home", )
-            break;
-    
-        case 'search':
-        
-            data = {title : "SEARCH" , color : 'blue' , itemId : "search"}
-            update (data)
-            history.pushState(data, "search", )
-            break;
-        
-        case 'likes':
-        
-            data = {title : "LIKES" , color : 'yellow' , itemId : "likes"}
-            update (data)
-            history.pushState(data, "likes", )
-            break;
-        
-        default:
-        
-            data = {title : "PROFILE" , color : 'pink' , itemId : "profile"}
-            update (data)
-            history.pushState(data, "profile", )
-            break;        
-    
-
-    }
+document.querySelector("nav").addEventListener("click", (e)=>{
+    if(e.target.nodeName != "I") return
+    // console.log(e.target.getAttributeNode("item-id").value)
    
+    let data;
+    switch (e.target.getAttributeNode("item-id").value){
+        case "home":
+            data = {title : "HOME", color : "red", itemId : "home"}
+            update(data)
+            history.pushState(data, "home", "home")
+            break;
+        
+        case "search":
+            data = {title : "SEARCH", color : "blue", itemId : "search"}
+            update(data)
+            history.pushState(data, "search", "search")
+            break;
 
-})
+        case "likes":
+            data = {title : "LIKES", color : "yellow", itemId : "likes"}
+            update(data)
+            history.pushState(data, "likes", "likes")
+            break;
 
-
-window.addEventListener('popstate', e =>{
-    if (history.state){
-        update(history.state)        
-    }   
-    else{
-        update({title:"HOME", color : 'red', itemId : "home"})
+        case "profile":
+            data = {title : "PROFILES", color : "pink", itemId : "profile"}
+            update(data)
+            history.pushState(data, "profile", "profile")
+            break;
     }
 })
 
-
-
-function update (data){
-    
+function update(data){
     document.querySelector("h1").innerText = data.title;
-
-    document.querySelector('main').style.backgroundColor = data.color;
-
-    document.querySelectorAll('i').forEach(element=>{
-        element.style.color = '#666'
+    document.querySelector("main").style.backgroundColor = data.color
+    document.querySelectorAll("I").forEach((element) =>{
+        element.style.color = "#666"
     })
-
-    document.querySelector(`i[item-id=${data.itemId}]`).style.color = 'black'
-
+    document.querySelector(`i[item-id = ${data.itemId}]`).style.color = "black"
 }
